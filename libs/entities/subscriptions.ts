289fad10a6e9020f9df82fb5327414c89e1ce584
Scott-Lasertrade <scott@lasertrade.com.au>
2022-04-05 15:12:34 +1000

    No blantant shared code, using libs instead

--- libs/entities/subscriptions.ts
+++
@@ -1,7 +0,0 @@
-// Subscribers
-export * from './subscribers/ManufacturerViewUpdateSubscriber';
-export * from './subscribers/ProductViewUpdateSubscriber';
-export * from './subscribers/ProductImageUpdateSubscriber';
-export * from './subscribers/OptimisticLockingSubscriber';
-export * from './subscribers/OfferHistoryAddition';
-export * from './subscribers/OrderNumberInsertSubscriber';
