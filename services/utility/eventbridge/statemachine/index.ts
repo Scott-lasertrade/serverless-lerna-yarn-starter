9d423665854a5b3b23d3eafbdabb8a713dc446cc
Scott-Lasertrade <scott@lasertrade.com.au>
2022-04-06 15:09:37 +1000

    Event bridge moved step, see if can release.
    If complains about event bus overlap, build arn

--- services/utility/eventbridge/statemachine/index.ts
+++ services/utility/eventbridge/statemachine/index.ts
@@ -1,13 +1,8 @@
-//  _________________________
-// |-------- Public ---------|
-// |_________________________|
-//      Category
 import expire_offer from './expire_offer';
 import refresh_listings from './refresh_listings';
 import reinstate_listings from './reinstate_listings';
 
 const functions = {
-    // Public
     expire_offer,
     refresh_listings,
     reinstate_listings,
