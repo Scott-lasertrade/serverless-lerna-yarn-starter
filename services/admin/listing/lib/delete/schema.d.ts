1a68dbb38368c839f1d2df62c04762fd4e6bed58
Scott-Lasertrade <scott@lasertrade.com.au>
2022-04-11 15:46:47 +1000

    admin listings for v3

---
+++ services/admin/listing/lib/delete/schema.d.ts
@@ -0,0 +1,11 @@
+declare const _default: {
+    readonly type: "object";
+    readonly properties: {
+        readonly version: {
+            readonly type: "number";
+        };
+    };
+    readonly required: readonly ["version"];
+};
+export default _default;
+//# sourceMappingURL=schema.d.ts.map
---
+++ services/admin/listing/lib/index.d.ts
@@ -0,0 +1,6 @@
+declare const functions: {
+    approval: any;
+    remove: any;
+};
+export default functions;
+//# sourceMappingURL=index.d.ts.map
