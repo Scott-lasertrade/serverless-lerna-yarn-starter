cc9c4852ccd9704c7bff6b0068890fb85f8545ea
Scott-Lasertrade <scott@lasertrade.com.au>
2022-04-11 13:59:32 +1000

    Try range of versions

--- services/mainService/serverless.ts
+++ services/mainService/serverless.ts
@@ -19,7 +19,7 @@ const serverlessConfiguration: AWS = {
     app: BaseServiceName,
     useDotenv: true,
     disabledDeprecations: ['CLI_OPTIONS_SCHEMA'],
-    frameworkVersion: '2.30.3',
+    frameworkVersion: '>=2.0.0 <3.0.0',
     custom: {
         ...SharedConfig,
         output: {
